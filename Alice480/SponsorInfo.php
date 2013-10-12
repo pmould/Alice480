148e6ec4130bcbfb982f2273f4f05c5784c565c8
pmould <paul_mould@hotmail.com>
2013-10-12 15:59:24 -0400

    updated page

--- Alice480/SponsorInfo.php
+++ Alice480/SponsorInfo.php
@@ -1,32 +1,32 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Bootstrap 101 Template</title>
+<meta charset="utf-8">
+	<title>Alice Website</title>
  
 	<link rel="stylesheet" type="text/css" href="mystyle.css">
 
 	
 	<style>
-	body
-	{
+	body {
 		font-family: "helvetica";
 		text-align: center;
-	
 	}
-	h1
-	{
+	
+	h1 {
 		font-family: "courier new";
 		font-size: 25px;
 		color: green;
 	}
 	
+	form {
+		display: block;
+		margin-top: 0em;
+	}
 	</style>
-	
-	
-	
+
   </head>
-  
-  
+ 
   <body>
     <h1>Welcome to the Alice Programming Contest!</h1>
 	<br>
@@ -40,12 +40,18 @@
 	<br><br>
 	Sponsor Information<br><br>
 	
-	<form>
+	<form id =form>
 	School: <input type="text" name="schoolName"><br>
 	City: <input type="text" name="city"><br>
 	State: <input type="text" name="state"><br>
 	Zip code: <input type="text" name="zipcode"><br>
-	Phone number:<input type="text" name="phonework"><br>
+	Phone number:
+	<input type="text" maxlength="3" style ="width:30px" name="phonepart1">
+	- 
+	<input type="text" maxlength="3" style ="width:30px" name="phonepart2">
+	- 
+	<input type="text" maxlength=
+	"4" style ="width:40px" name="phonepart3">
 	<input type="submit" name="Submit"><br>
 	</form>
 
